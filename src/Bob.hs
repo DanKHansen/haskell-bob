863e3974aa9fa9d37876a3aa6b4fafa83873b054
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-07 13:18:53 +0200

    Solution with case of

--- src/Bob.hs
+++ src/Bob.hs
@@ -1,16 +1,16 @@
 module Bob (responseFor) where
 
-import Data.Char
+import Data.Char (isLetter, isSpace, isUpper)
 
 responseFor :: String -> String
-responseFor xs
-  | null trimmedText = "Fine. Be that way!"
-  | isShouting && isQuestion = "Calm down, I know what I'm doing!"
-  | isShouting = "Whoa, chill out!"
-  | isQuestion = "Sure."
-  | otherwise = "Whatever."
+responseFor xs = case () of
+  _
+    | null trimmed -> "Fine. Be that way!"
+    | isShouting && isQuestion -> "Calm down, I know what I'm doing!"
+    | isShouting -> "Whoa, chill out!"
+    | isQuestion -> "Sure."
+    | otherwise -> "Whatever."
   where
-    trimmedText = filter (not . isSpace) xs
-    onlyLetters = filter isLetter trimmedText
-    isShouting = all isUpper onlyLetters && any isUpper onlyLetters
-    isQuestion = last trimmedText == '?'
+    trimmed = filter (not . isSpace) xs
+    isShouting = all isUpper (filter isLetter trimmed) && any isUpper (filter isLetter trimmed)
+    isQuestion = last trimmed == '?'
