50cf51472f81921bd950508a3b836926b1bca802
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-07 13:06:45 +0200

    Solution

--- src/Bob.hs
+++ src/Bob.hs
@@ -1,4 +1,16 @@
 module Bob (responseFor) where
 
+import Data.Char
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | null trimmedText = "Fine. Be that way!"
+  | isShouting && isQuestion = "Calm down, I know what I'm doing!"
+  | isShouting = "Whoa, chill out!"
+  | isQuestion = "Sure."
+  | otherwise = "Whatever."
+  where
+    trimmedText = filter (not . isSpace) xs
+    onlyLetters = filter isLetter trimmedText
+    isShouting = all isUpper onlyLetters && any isUpper onlyLetters
+    isQuestion = last trimmedText == '?'
